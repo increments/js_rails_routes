9f528f94aadf693a60e38a634938175b4bcdd15a
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-06-25 13:21:31 +0900

    Add JSRailsRoutes.configure

--- lib/js_rails_routes.rb
+++ lib/js_rails_routes.rb
@@ -1,3 +1,11 @@
 require 'js_rails_routes/engine'
 require 'js_rails_routes/generator'
 require 'js_rails_routes/version'
+
+module JSRailsRoutes
+  module_function
+
+  def configure(&block)
+    block.call(Generator.instance)
+  end
+end
---
+++ spec/js_rails_routes_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe JSRailsRoutes do
+  describe '.configure' do
+    it 'yields with Generator instance' do
+      expect { |b| described_class.configure(&b) }.to yield_with_args(JSRailsRoutes::Generator.instance)
+    end
+  end
+end
